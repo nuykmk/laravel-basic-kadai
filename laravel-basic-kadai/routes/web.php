bc0b945d32d87c5e3c7022f3c3df531d7fb18cd7
nuykmk <yy.uni.yy.yy@gmail.com>
2025-08-17 08:37:04 +0900

    24章課題提出

--- laravel-basic-kadai/routes/web.php
+++ laravel-basic-kadai/routes/web.php
@@ -20,6 +20,9 @@ use App\Http\Controllers\PostController; //PostController を使えるように�
 // Route::get('/', function () {
 //     return view('welcome');
 // });
+Route::get('/posts/create', [PostController::class, 'create']);
+Route::post('/posts/store', [PostController::class, 'store'])->name('posts.store');
+
 
 Route::get('/posts', [PostController::class, 'index']);
 
