cf012464e4d428b1ca021206ba7ed594249db64d
nuykmk <yy.uni.yy.yy@gmail.com>
2025-08-21 10:05:47 +0900

    課題Laravel Breezeで認証を実装しよう提出

--- laravel-basic-kadai/routes/web.php
+++ laravel-basic-kadai/routes/web.php
@@ -1,11 +1,10 @@
 <?php
+
+use App\Http\Controllers\ProfileController;
 //「ルート定義に使う Route クラスを読み込みますよ！」
 use Illuminate\Support\Facades\Route;     //Route クラスを使えるようにする
-
 //「PostControllerを使いますよ！」とLaravelに伝える宣言
 use App\Http\Controllers\PostController; //PostController を使えるようにする
-
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,11 +16,25 @@ use App\Http\Controllers\PostController; //PostController を使えるように�
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-Route::get('/posts/create', [PostController::class, 'create']);
-Route::post('/posts/store', [PostController::class, 'store'])->name('posts.store');
+Route::get('/', function () {
+  return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+  return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+  Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+  Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+  Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__ . '/auth.php';
+
+
+Route::get('/posts/create', [PostController::class, 'create'])->middleware('auth');
+Route::post('/posts/store', [PostController::class, 'store'])->name('posts.store')->middleware('auth');
 
 
 Route::get('/posts', [PostController::class, 'index']);
